8f8e5aaf3fed1c270280bdaf61696a6039f56f43
coderoque <coderoque@gmail.com>
2022-11-07 00:20:15 +0400

    WIP

--- bot/commands/descriptions/__init__.py
+++ bot/commands/descriptions/__init__.py
@@ -1,7 +1,20 @@
 CMD_DESC = (
-    # cmd, inline description, long description
-    ('start', "let's have some fun", "an idle brain is the devil's workshop"),
-    ('help', 'help and how-to',
-     'you know how to use this command if you are reading this message :)'),
-    ('whoami', 'get your account id', None),
+    """
+    cmd, inline description, long description
+    """
+    # /-commands (bot info commands)
+    # not for use in group chat
+    ('start', "▶ let's have some fun baby ☣",
+     "...an idle brain is the devil's workshop 😈"),
+    ('help', '📑 help and how-to',
+     'you know how to use this command if you are reading this message :)\n\n'
+     'another way to get info about command is inline buttons in this chat (NOT IN GROUP CHAT)'),
+    ('whoami', 'get your telegram account id 👨‍💻', None),
+    # !-commands (moderator's commands)
+    # for use in group chat only
+    ('rr', "random read-only",
+     ''),
+    # $-commands
+    # %-commands
+    # &-commands
 )
